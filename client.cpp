e9a3d16be82419391de05e443b07c4cf03c88ab2
M G <fishtez@ucla.edu>
2018-07-19 07:46:27 -0700

    fixing indents round 2

--- client.cpp
+++ client.cpp
@@ -16,54 +16,55 @@
 #include <unistd.h> /* close */
 
 int main( int argc, char *argv[] ){
-  if (argc < 4) {
-    std::cerr << "Required arguments: hostname, server port number, name of requested file.\n";
-    exit(1);
-  }
-  
-  char* hostname = argv[1];
-  char* portnum = argv[2];
-  char* filename = argv[3];
-  // prints message and exits if out of range.
-  valid_portnum(portnum);
-
-  // set up socket
-  int sockfd, rv, numbytes;
-  struct addrinfo hints, *servinfo, *p;
-
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_DGRAM;
-
-  if ((rv = getaddrinfo(hostname, portnum, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-    return 1;
-  }
-
-  // loop through all the results and make a socket
-  for(p = servinfo; p != NULL; p = p->ai_next) {
-    if ((sockfd = socket(p->ai_family, p->ai_socktype,p->ai_protocol)) == -1) {
-      perror("talker: socket");
-      continue;
+    if (argc < 4) {
+        std::cerr << "Required arguments: hostname, server port number, name of requested file.\n";
+        exit(1);
     }
-    break;
-  }
 
-  if (p == NULL) {
-    fprintf(stderr, "talker: failed to create socket\n");
-    return 2;
-  }
+    char* hostname = argv[1];
+    char* portnum = argv[2];
+    char* filename = argv[3];
+    // prints message and exits if out of range.
+    valid_portnum(portnum);
 
-  if ((numbytes = sendto(sockfd, filename, strlen(filename), 0,
-      p->ai_addr, p->ai_addrlen)) == -1) {
-    perror("talker: sendto");
-    exit(1);
-  }
+    // set up socket
+    int sockfd, rv, numbytes;
+    struct addrinfo hints, *servinfo, *p;
 
-  freeaddrinfo(servinfo);
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
 
-  printf("talker: sent %d bytes to %s\n", numbytes, hostname);
-  close(sockfd);
+    if ((rv = getaddrinfo(hostname, portnum, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
 
-  return 0;
+    // loop through all the results and make a socket
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,p->ai_protocol)) == -1) {
+            perror("talker: socket");
+            continue;
+        }
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to create socket\n");
+        return 2;
+    }
+
+    if ((numbytes = sendto(sockfd, filename, strlen(filename),
+        0,p->ai_addr, p->ai_addrlen)) == -1) {
+
+        perror("talker: sendto");
+        exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("talker: sent %d bytes to %s\n", numbytes, hostname);
+    close(sockfd);
+
+    return 0;
 } //end main()
